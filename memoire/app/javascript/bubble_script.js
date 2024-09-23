749eac5b5bcd3767c90893fc9e988f70e0489048
田中元気 <cavacan@Cavacan-MBP.local>
2024-09-23 15:23:09 +0900

    add func newBubble updateBubble

--- memoire/app/javascript/bubble_script.js
+++ memoire/app/javascript/bubble_script.js
@@ -1,3 +1,4 @@
+export let currentBubble = null; 
 export function renderBubbles(bubblesData) {
   
   document.addEventListener('DOMContentLoaded', function() {
@@ -24,7 +25,7 @@ export function renderBubbles(bubblesData) {
 
       currentLeft += bubbleBetweenWidth;
       if (currentLeft > pageWidth - bubbleBetweenWidth){ // 画面外描画を防ぐためマージンを多めに確保。
-        currentLeft = 0;
+        currentLeft = 80;
       }
     
       // フェードイン
@@ -40,8 +41,30 @@ export function renderBubbles(bubblesData) {
 
       // text表示を設定
       bubbleElement.addEventListener('click', () => {
+        currentBubble = index;
         textWindow.textContent = bubbleData.text;
       })
+
+      // text-windowにクリックで編集画面呼び出す機能を追加
+      textWindow.addEventListener('click', () => {
+        if (document.querySelector('.edit-window')) {
+          return;
+        }else if (currentBubble != null) {
+          createEditWindow(bubblesData[currentBubble]);
+        }
+      })
+    });
+
+    // 思い出登録用bubble
+    const newBubbleElement = document.createElement('div');
+    newBubbleElement.classList.add('newBubble');
+    newBubbleElement.textContent = '+';
+    newBubbleElement.style.left = '90%';
+    newBubbleElement.style.top = '55%';
+    document.body.appendChild(newBubbleElement);
+
+    newBubbleElement.addEventListener('click', () => {
+      createBubbleWindow();
     });
   });
 }
@@ -74,3 +97,173 @@ function createBounceAnimation(bubbleElement, randomTop) {
   // アニメーションを適用
   bubbleElement.style.animation = `${animationName} 4s infinite ease-in-out`;
 }
+
+// 編集機能　一式
+function createEditWindow(bubbleData) {
+  const editWindow = document.createElement('div');
+  editWindow.classList.add('edit-window');
+
+  // edit画面を作成
+  editWindow.innerHTML = `
+  <h3 style="text-align: center; margin-bottom: 5px;">思い出を変える</h3>
+  <form id='editForm'>
+    <div style="margin-bottom: 10px;">
+      <label for='title'>タイトル:</label>
+      <input type='text' name='title' value="${bubbleData.title}" style="width: 100%;"/>
+    </div>
+    <div style="margin-bottom: 10px;">
+      <label for='text'>内容:</label>
+      <textarea name='text' style="width: 100%; height: 100px;">${bubbleData.text}</textarea>
+    </div>
+    <div style="margin-top: 10px; text-align: right;">
+      <button type='submit'>上書き</button>
+      <button type='button' class='cancelButton'>取り消し</button>
+    </div>
+  </form>
+  `;
+  document.body.appendChild(editWindow);
+
+  editWindow.style.display = 'block';
+
+  // キャンセルボタンにedit画面削除を追加
+  editWindow.querySelector('.cancelButton').addEventListener('click', () => {
+    editWindow.remove();
+  });
+
+  // Saveボタン　更新
+  document.getElementById('editForm').addEventListener('submit', (e) => {
+    // 画面の再読み込みを阻止
+    e.preventDefault();
+
+    // 編集内容を取得
+    const title = editWindow.querySelector('input[name="title"]').value;
+    const text = editWindow.querySelector('textarea[name="text"]').value;
+
+    updateBubble(currentBubble, { title, text });
+    editWindow.remove();
+  });
+}
+
+// fetchを使ってデータを更新
+function updateBubble(bubbleId, updateData) {
+  fetch('/bubbles/${bubbleId}', {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json',
+      'X-CSRF-Token': document.querySelector('meta[name="csrf-token"]').getAttribute('content'),
+    },
+    body: JSON.stringify(updateData),
+  })
+  .then(response => response.json())
+  .then(data => {
+    const editWindow = document.querySelector('.edit-window');
+    if (editWindow) {
+      editWindow.remove();
+    }
+
+    // 表示されているbubbleのタイトルを更新
+    document.querySelector('.bubble[data-index="${bubbleId}"]').textContent = data.title;
+
+    // text-window画面のtextを更新
+    const textWindow = document.querySelector('#text-window');
+    textWindow.textContent = data.text;
+  })
+  .catch(error => {
+    console.log('Error updating Memory: ', error);
+  });
+}
+
+// 新規思い出登録
+function createBubbleWindow() {
+  if (document.querySelector('.create-window')) {
+    return;
+  }
+  const createWindow = document.createElement('div');
+  createWindow.classList.add('create-window');
+
+  // 作成画面を作成
+  createWindow.innerHTML = `
+  <h3 style="text-align: center; margin-bottom: 5px;">思い出を作成</h3>
+  <form id='createForm'>
+    <div style="margin-bottom: 10px;">
+      <label for='title'>タイトル:</label>
+      <input type='text' name='title' style="width: 100%;" required/>
+    </div>
+    <div style="margin-bottom: 10px;">
+      <label for='text'>内容:</label>
+      <textarea name='text' style="width: 100%; height: 100px;" required></textarea>
+    </div>
+    <div style="margin-top: 10px; text-align: right;">
+      <button type='submit'>思い出を作る</button>
+      <button type='button' class='cancelNew'>取り消し</button>
+    </div>
+  </form>
+  `;
+  document.body.appendChild(createWindow);
+
+  // キャンセルボタンに新規登録画面削除を追加
+  document.querySelector('.cancelNew').addEventListener('click', () => {
+    createWindow.remove();
+  });
+
+  // Saveボタン　更新
+  document.getElementById('createForm').addEventListener('submit', (e) => {
+    // 画面の再読み込みを阻止
+    e.preventDefault();
+
+    // 作成内容を取得
+    const title = createWindow.querySelector('input[name="title"]').value;
+    const text = createWindow.querySelector('textarea[name="text"]').value;
+
+    createBubble( { title, text });
+    createWindow.remove();
+  });
+}
+
+function createBubble(newData) {
+  fetch('/bubbles', {
+    method: 'POST',
+    header: {
+      'Content-Type': 'application/json',
+      'X-CSRF-Token': document.querySelector('meta[name="csrf-token"]').getAttribute('content'),
+    },
+    body: JSON.stringify(newData),
+  })
+  .then(response => response.json())
+  .then(data => {
+    addNewBubbleToPage(data);
+  })
+  .catch(error => {
+    console.log('Error creating bubble :', error);
+  });
+}
+
+function addNewBubbleToPage(bubbleData) {
+  const textWindow = document.querySelector('#text-window');
+  let currentLeft = 80
+
+  const newBubbleElement = document.createElement('div');
+  newBubbleElement.classList.add('bubble');
+  newBubbleElement.dataset.id = bubbleData.id;
+  newBubbleElement.textContent = bubbleData.title;
+
+  const randomTop = Math.random() * 50 + 5;
+  newBubbleElement.style.transform = `translateY(100vh)`;
+  newBubbleElement.style.left = `${currentLeft}px`;
+  document.body.appendChild(newBubbleElement);
+
+  setTimeout(() =>{
+    newBubbleElement.style.transform = `translateY(${randomTop}vh)`;
+    newBubbleElement.style.opacity = '1';
+  }, 100)
+
+  setTimeout(() => {
+    createBounceAnimation(newBubbleElement, randomTop);
+  }, 3000);
+
+  newBubbleElement.addEventListener('click', () => {
+    currentBubble = bubbleData.id;
+    textWindow.textContent = bubbleData.text;
+  });
+}
+
